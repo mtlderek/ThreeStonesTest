ec23ebcb5793db8dd0d122f3496e9f832713d0ca
Sebastian Ramirez <sebastian.ramirez.p@hotmail.com>
2017-10-29 14:20:36 -0400

    Javadocs for the Board class

--- 3StonesTest/src/three_stones/views/Board.java
+++ 3StonesTest/src/three_stones/views/Board.java
@@ -1,9 +1,24 @@
-
 package three_stones.views;
 
 //import ThreeStones_Game.enums.Turn;
 import java.util.ArrayList;
 import java.util.List;
+/**
+ * This class represents a Board and all its components needed to play
+ * the 3 stones game. A Board is composed of a gameBoard, a two dimensional array
+ * of 11 by 11. The board is made to be 11 by 11 to not run into an 
+ * IndexOutOfBounds exception. Each slot in the array is represented by a number
+ * that will dictate the state each slot is in. 0 represents a slot where either
+ * the server or the client can make a move in. 1 represents a slot where the client
+ * has already made a move in. 2 represents a slot where the server has made a 
+ * move in. 9 represents a space where making a move in is impossible. The board
+ * class also keeps track of the player score, the server score and the last slot
+ * where a move was made in.
+ *
+ * @author Derek McLean
+ * @author Sebastian Ramirez
+ * @author Johnny Lin
+ */
 
 public class Board{
 //    private Turn turn;
@@ -14,7 +29,11 @@ public class Board{
     private int playerScore;
     private int serverScore;
     private int player;
-
+/**
+ * No parameters, default constructor. It initializes a 2 dimensional array representing the
+ * game board, the number of stones left to play and initializes the array that will
+ * hold the last played move.
+ */
     public Board() {
         int[][] gameboard = {
         {9,9,9,9,9,9,9,9,9,9,9},
@@ -47,9 +66,12 @@ public class Board{
         this.lastPlayed = new int[2];
     }
     /**
-     * Gets any position where the current number is zero
+     * Gets all the slots available to play after either the client or the server
+     * have made a move. This method is expected to return all the empty spaces
+     * in the column and the row where the last move was played.
      * @param lastPlayed
-     * @return 
+     * @return A list containing all the available slots where either the client
+     * or the server can make a move in.
      */
     public List<int[]> getAvailable(int[] lastPlayed){
         System.out.println("Last Played: "+lastPlayed[0]+" "+lastPlayed[1]);
@@ -76,6 +98,12 @@ public class Board{
         }
         return available;
     }
+    /**
+     * The toString method of the Board class has been overridden in order to
+     * represent the actual state of the gameBoard throughout the game between
+     * the server and the client.
+     * @return A string representation of the Board.
+     */
     @Override
     public String toString(){
         //TODO: make it so 9s dont show, and 1 become W, and 2 becomes B
@@ -106,9 +134,9 @@ public class Board{
     }
     
     /**
-     * Updates the board and last played string after most recent move
-     * 
-     * 
+     * The updateBoard method is used to update all the variables in the Board
+     * class after a move has been made. It updates the current player making a
+     * move as well as the last played slot and the current score for both players.
      * @param player
      * @param position
      */
@@ -122,7 +150,10 @@ public class Board{
         System.out.println("Score:\rPlayer: "+playerScore+" Server: "+serverScore);
 
     }
-    
+    /**
+     * Helper method utilized to clear the game board after either the client
+     * decides to start a new game or the game is finished to completion.
+     */
     public void reset(){
         int [][] gameboard = {
         {9,9,9,9,9,9,9,9,9,9,9},
@@ -145,7 +176,13 @@ public class Board{
         serverScore = 0;
         
     }
-    
+    /**
+     * Gets all the slots available to play after either the client or the server
+     * have made a move. This method is expected to return all the empty spaces
+     * in the column and the row where the last move was played.
+     * @return A list containing all the available slots where either the client
+     * or the server can make a move in.
+     */
     public List<int[]> getAvailableMoves(){
         List<int[]> moves = new ArrayList<>();
         
@@ -158,10 +195,17 @@ public class Board{
         }
         return moves;
     }
-    
+    /**
+     * Returns the last played slot.
+     * @return The slot where the last played move was in.
+     */
     public int[] getLastPlayed(){
         return lastPlayed;
     }
+    /**
+     * This method is called whenever the game board is update. It checks which
+     * player just played and sets the score according to it.
+     */
     public void setScore(){
         int x = lastPlayed[1];
         int y = lastPlayed[0];
@@ -177,6 +221,14 @@ public class Board{
         }
 //        printScore();
     }
+    /**
+     * This method encompasses the four different check methods needed to 
+     * accurately assign points to each player after a move has been played.
+     * @param x
+     * @param y
+     * @return An int representing the total points a player has scored after
+     * a move has been played.
+     */
         public int checkScore(int x, int y){ //reverse x and y
         int totalMoveScore = 0;
         totalMoveScore += checkDiagonalLR(x,y);
@@ -185,7 +237,13 @@ public class Board{
         totalMoveScore += checkHorizontal(x,y);
         return totalMoveScore;
     }
-    
+    /**
+     * This method checks for points in a diagonal that goes left to right.
+     * @param x
+     * @param y
+     * @return An int representing the total points a player has scored in a 
+     * left to right diagonal.
+     */
     private int checkDiagonalLR(int x, int y){
         int currentScore = 0;
         if(gameBoard[x-2][y-2]==player&&gameBoard[x-1][y-1]==player){
@@ -199,6 +257,13 @@ public class Board{
         }
         return currentScore;
     }
+        /**
+     * This method checks for points in a diagonal that goes right to left.
+     * @param x
+     * @param y
+     * @return An int representing the total points a player has scored in a 
+     * right to left diagonal.
+     */
     private int checkDiagonalRL(int x, int y){
         int currentScore = 0;
         if(gameBoard[x+2][y-2]==player&&gameBoard[x+1][y-1]==player){
@@ -212,6 +277,13 @@ public class Board{
         }
         return currentScore;
     }
+    /**
+     * This method checks for points in a vertical line.
+     * @param x
+     * @param y
+     * @return An int representing the total points a player has scored in a 
+     * vertical line.
+     */
     private int checkVertical(int x, int y){
         int currentScore = 0;
         if(gameBoard[x][y+2]==player&&gameBoard[x][y+1]==player){
@@ -225,6 +297,13 @@ public class Board{
         }
         return currentScore;
     }
+        /**
+     * This method checks for points in an horizontal line.
+     * @param x
+     * @param y
+     * @return An int representing the total points a player has scored in an 
+     * horizontal line.
+     */
     private int checkHorizontal(int x, int y){
         int currentScore = 0;
         if(gameBoard[x-2][y]==player&&gameBoard[x-1][y]==player){
@@ -238,31 +317,57 @@ public class Board{
         }
         return currentScore;
     }
-    
+    /**
+     * This method prints the respective scores to the screen.
+     */
     public void printScore(){
         System.out.println("Score:\rPlayer: "+playerScore+" Server: "+serverScore);
     }
-    
+    /**
+     * This method returns the points the server has currently attained.
+     * @return The current score of the server. 
+     */
     public int getServerScore(){
         return serverScore;
     }
-    
+    /**
+     * This method returns the points the client has currently attained.
+     * @return The current score of the client. 
+     */
     public int getPlayerScore(){
         return playerScore;
     }
-    
+    /**
+     * Sets the lastPlayed attribute according to the last move registered by
+     * the server.
+     * @param lastPlayed 
+     */
     public void setLastPlayed(int[] lastPlayed) {
         this.lastPlayed = lastPlayed;
     }
-    
+    /**
+     * Sets the player parameter according to the player currently playing.
+     * 1 for the client and 2 for the server.
+     * @param player 
+     */
     public void setPlayer(int player){
         this.player = player;
     }
-    
+    /**
+     * Returns the current player.
+     * @return The player currently player. Client is 1 and Server is 2.
+     */
     public int getPlayer() {
         return player;
     }
-    
+    /**
+     * Returns the int value at the specific slot denoted by its x and y 
+     * coordinates.
+     * @param x
+     * @param y
+     * @return 0 if the slot is empty, 9 if the slot is not playable, 1 if the
+     * client has made a move in the slot and 2 if the server has made a move in the slot.
+     */
     public int getValueAt(int x, int y) {
 //        System.out.println("TEST: Board.getValueAt(): " + x + " " + y);
         int value = gameBoard[y][x]; //may need to swap x,y
